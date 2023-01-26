26e0566de621785ea0d31807f66bb415e030fc7c
Anton Demidenko <yaintroversivnost@gmail.com>
2023-01-26 14:13:08 +0500

    Обновление сборки

--- js/app.js
+++ js/app.js
@@ -1,6 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-
-console.log('START');
-
-
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('START');
 }); // Конец DOMContentLoaded
